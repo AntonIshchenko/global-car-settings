e6b641c80ed43762b5777bb99e7032df90176542
Anton Ishchenko <anton_ishchenko@epam.com>
2020-01-16 18:25:39 +0300

    initial commit

---
+++ src/main/java/com/epam/globalcarsettings/GlobalCarSettingsApplication.java
@@ -0,0 +1,13 @@
+package com.epam.globalcarsettings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalCarSettingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalCarSettingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/globalcarsettings/GlobalCarSettingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.globalcarsettings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalCarSettingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
